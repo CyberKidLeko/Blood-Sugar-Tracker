dd3b1709a6b0ad646b28bdb1ba92d67fd3f1fd0a
CyberKidLeko <nkululekongwenya123@gmail.com>
2024-10-07 01:53:55 +0200

    -hi there, this is a branch where i attempted to fix your issue -feel free to add css styling to the link if you like

--- assets/script.js
+++ assets/script.js
@@ -8,36 +8,39 @@ const addReading = document.getElementById('add_reading')
 // create function to add row each time event listener is being clicked
 
 function addRow() {
-    for (let i = 0; i < currentRow; i++) {
-        console.log(currentRow)
+    //remove the for loop
+    //no for loop means that the function can only create one row per click
+    console.log(currentRow)
         // variables to create new row
-        const addNewRow = document.createElement('tr')
-        const dateData = document.createElement('td')
-        const timeData = document.createElement('td')
-        const readingData = document.createElement('td')
+    const addNewRow = document.createElement('tr')
+    const dateData = document.createElement('td')
+    const timeData = document.createElement('td')
+    const readingData = document.createElement('td')
         
-        addNewRow.setAttribute(
-            'style', 
-            'border: 1px solid black; height: 25px;'
-        )
-        dateData.setAttribute(
-            'style',
-            'border: 1px solid black;'
-        )
+    addNewRow.setAttribute(
+        'style', 
+        'border: 1px solid black; height: 25px;'
+    )
+    dateData.setAttribute(
+        'style',
+        'border: 1px solid black;'
+    )
 
-        timeData.setAttribute(
-            'style',
-            'border: 1px solid black;'
-        )
+    timeData.setAttribute(
+        'style',
+        'border: 1px solid black;'
+    )
 
-        readingData.setAttribute(
-            'style',
-            'border: 1px solid black;'
-        )
-        addNewRow.append(dateData, timeData, readingData)
-        currentRow.append(addNewRow)
-    }
+    readingData.setAttribute(
+        'style',
+        'border: 1px solid black;'
+    )
+    addNewRow.append(dateData, timeData, readingData)
+    currentRow.append(addNewRow)
+    
 }
 
 // event listener to make the button functional
-addReading.addEventListener('click', addRow())
+addReading.addEventListener('click', addRow)
+//you shouldnt call the addRow method. just reference it
+//this makes sure it doesnt immediately call the method, but waits till the event occurs(button is clicked)
