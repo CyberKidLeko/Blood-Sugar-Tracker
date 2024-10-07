f24368d587b146f3e9282c796f236ba87c89ac43
CyberKidLeko <nkululekongwenya123@gmail.com>
2024-10-07 02:19:12 +0200

    buttons fixed

--- assets/script.js
+++ assets/script.js
@@ -8,14 +8,17 @@ const addReading = document.getElementById('add_reading')
 // create function to add row each time event listener is being clicked
 
 function addRow() {
-    // variables to create new row
+    //remove the for loop
+    //no for loop means that the function can only create one row per click
+    console.log(currentRow)
+        // variables to create new row
     const addNewRow = document.createElement('tr')
     const dateData = document.createElement('td')
     const timeData = document.createElement('td')
     const readingData = document.createElement('td')
-
+        
     addNewRow.setAttribute(
-        'style',
+        'style', 
         'border: 1px solid black; height: 25px;'
     )
     dateData.setAttribute(
@@ -34,7 +37,10 @@ function addRow() {
     )
     addNewRow.append(dateData, timeData, readingData)
     currentRow.append(addNewRow)
+    
 }
 
 // event listener to make the button functional
 addReading.addEventListener('click', addRow)
+//you shouldnt call the addRow method. just reference it
+//this makes sure it doesnt immediately call the method, but waits till the event occurs(button is clicked)
